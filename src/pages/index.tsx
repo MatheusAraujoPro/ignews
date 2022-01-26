e04cb34e95033940e7663b618e87a451cf59d9b0
Matheus Araujo <matheus.a@havan.com.br>
2022-01-26 13:35:30 -0300

    Title dinâmico por página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,18 @@
+import Head from "next/head";
 import styles from "../../styles/home.module.scss";
+
 export default function Home() {
-  return <h1 className={styles.title}>Hello World</h1>;
+  return (
+    <>
+      {/* 
+        Cabeçalho dinâmico 
+        Em qualque lugar da Aplicação é possível setar
+        propriedades como a tag title dentro do Head.
+      */}
+      <Head>
+        <title>Ig.News | Inicio</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
+  );
 }
