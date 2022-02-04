077c06557f2de7272880a5e6a98ba0d1771c200a
Matheus Araujo <matheus.a@havan.com.br>
2022-02-04 15:25:10 -0300

    Salvando usuário no Banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configurando a integração com o Provider do Github
@@ -10,12 +13,30 @@ export default NextAuth({
       authorization:{
         params:{
           scope:'read:user'
-        }
-      }
-    
-    }),
-    
-    // ...add more providers here
-    
+        }        
+      },
+          
+    },)   
+    // ...add more providers here    
   ],
+  callbacks:{
+    async signIn({ user }) {  
+      const { email } = user
+      try {
+        await fauna.query
+        (
+          q.Create
+          (
+            q.Collection('users'), { data: { email } }
+          )
+        )
+        return true
+
+      } catch (err) {        
+        console.log(err)
+        
+        return false
+      }
+    }
+  }
 })
