d01dcf53a27034d7aed900780deb145fd923846f
Matheus Araujo <matheus.a@havan.com.br>
2022-02-04 14:00:01 -0300

    Configurando FaunaDB

---
+++ src/services/fauna.ts
@@ -0,0 +1,14 @@
+import { Client } from 'faunadb'
+/*
+  Operações que que utilizam o FAUNA ou o STRIPE
+  que usem esta chave secreta não devem ser feitas
+  no lado do cliente. Ou seja, nenhum componente 
+  da aplicação deverá fazê-las. 
+
+  Essas operações deverão ser feitas apenas pelas chamadas
+  NEXT(SSR OU SSG), pelas API-ROUTES
+*/
+
+export const fauna = new Client({
+  secret: process.env.FAUNADB_KEY
+})
