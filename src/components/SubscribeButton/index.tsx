e340bd473b44ab999a389662511cc1b7397044e8
Matheus Araujo <matheus.a@havan.com.br>
2022-02-07 13:32:41 -0300

    Redirecionando para o Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -7,19 +9,30 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
     }
+    try {
+      // Chamada à API-ROUTE
 
-    //Criação da CheckoutSession
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      // Redirecinar o usuário
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
   }
   return (
     <button
       type="button"
       className={styles.subscribeButton}
-      onClick={() => handleSubscribe}
+      onClick={() => handleSubscribe()}
     >
       SubsCribe Now
     </button>
