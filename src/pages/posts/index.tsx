3fb6f594c3975102751845b377859daacb502828
Matheus Araujo <matheus.a@havan.com.br>
2022-02-25 15:09:57 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,21 @@ import Head from "next/Head";
 import { getPrismicClient } from "../../services/prismic";
 import Prismic from "@prismicio/client";
 import styles from "./styles.module.scss";
+import { RichText } from "prismic-dom";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps) {
+  console.log(posts);
   return (
     <>
       <Head>
@@ -13,29 +26,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Título do Post</strong>
-            <p>Parágrafo do Post</p>
-          </a>
-
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Título do Post</strong>
-            <p>Parágrafo do Post</p>
-          </a>
-
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Título do Post</strong>
-            <p>Parágrafo do Post</p>
-          </a>
-
-          <a href="">
-            <time>12 de março de 2021</time>
-            <strong>Título do Post</strong>
-            <p>Parágrafo do Post</p>
-          </a>
+          {posts.map((post) => (
+            <a key="" href="">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -52,9 +49,28 @@ export const getStaticProps: GetStaticProps = async () => {
       pageSize: 100,
     }
   );
-  console.log(JSON.stringify(response, null, 2));
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: {
+      posts,
+    },
   };
 };
