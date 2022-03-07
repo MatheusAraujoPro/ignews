a3a90e3ea673948336a80ec39841676ddc4165f6
Matheus Araujo <matheus.a@havan.com.br>
2022-03-07 07:19:53 -0300

    Componente: ActiveLink

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,19 @@
 import { SigniInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
-import Link from "next/link";
+
+import { ActiveLink } from "../ActiveLink";
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logo" />
         <nav>
-          <Link href="/">
+          <ActiveLink href="/" activeClassname={styles.isActive}>
             <a className={styles.isActive}>Home</a>
-          </Link>
-          <Link href="/posts" prefetch>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassname={styles.isActive} prefetch>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SigniInButton />
